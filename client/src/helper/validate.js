d5ccd7f2f79cc50cabcef7b66f11392f942caeed
Soumyajit Paria <soumyajitparia2005@gmail.com>
2023-04-15 11:44:58 +0530

    2nd commit

---
+++ client/src/helper/validate.js
@@ -0,0 +1,30 @@
+import toast from "react-hot-toast";
+
+/* validate login page username */
+export async function usernameValidate(values){
+    const errors = usernameVerify({}, values);
+
+    return errors;
+}
+
+// validate username 
+function usernameVerify(error={}, values){
+    if(!values.username){
+        error.username = toast.error("Username Required...!");
+    }else if(values.username.includes(" ")){
+        error.username = toast.error("Invalid Username...!");
+    }
+
+    return error;
+}
+
+// validate username 
+function passwordVerify(error={}, values){
+    if(!values.password){
+        error.password = toast.error("Password Required...!");
+    } else if(values.password.includes(" ")){
+        error.password = toast.error("Wrong Password...!");
+    }else if(values.password.length()<0){
+        error.password = toast.error("Invalid Password...!");
+    }
+}
